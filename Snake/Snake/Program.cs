3f92b22fcd067de20ace01034d651ecd1933c61a
Maks <52411040+Zinger69@users.noreply.github.com>
2020-04-06 18:48:12 +0300

    Add files via upload
    Инкапсуляция в классы чтобы код был чище и пользователь не видел что происходит в открытой часте кода.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,23 +6,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();   // чтобы не писать много строчек, создали класс и обявили его тут 
-            p1.x = 1;                          //(Вместо ****А***** написали только это) 
-            p1.y = 3;
-            p1.sym = '*';
-            p1.Draw(); // Вместо Draw(x1, y1, sym1);
+            Point p1 = new Point(1, 3, '*'); 
+            p1.Draw(); 
 
-
-            /* ****А*****
-            int x1 = 1;        // Переменные места куда ставим по х
-            int y1 = 3;        // Переменные места куда ставим по у
-            char sym1 = '*';   // Переменные которой задаем что ставим в ''
-            */
-
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '@';
+            Point p2 = new Point(4, 5, '@');
             p2.Draw();
 
             Console.WriteLine();
