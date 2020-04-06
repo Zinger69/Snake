520ddb37033109702364c27a7d462a17aeab682e
Maks <52411040+Zinger69@users.noreply.github.com>
2020-04-06 22:02:06 +0300

    Add files via upload
    Draw snake

---
+++ Snake/Snake/Snake.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Snake : Figure
+    {
+
+		Direction direction;
+
+		public Snake(Point tail, int length, Direction _direction)
+		{
+			direction = _direction;
+			pList = new List<Point>();
+			for (int i = 0; i < length; i++)
+			{
+				Point p = new Point(tail);
+				p.Move(i, direction);
+				pList.Add(p);
+			}
+		}
+	}
+}
