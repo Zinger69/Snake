d0c2c222be4376c08f06134b0ab689854f237e28
Maks <52411040+Zinger69@users.noreply.github.com>
2020-04-06 17:43:01 +0300

    Add files via upload
    Функции вместо повторяющихся кусков кода.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,8 +6,24 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1;        // Переменные места куда ставим по х
+            int y1 = 3;        // Переменные места куда ставим по у
+            char sym1 = '*';   // Переменные которой задаем что ставим в ''
+
+            Draw(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '@';
+
+            Draw(x2, y2, sym2);
+
             Console.WriteLine();
+        }
+        static void Draw(int x, int y, char sym) // Функция вывода на экран
+        {
+            Console.SetCursorPosition(x, y);       
+            Console.Write(sym);
         }
     }
 }
