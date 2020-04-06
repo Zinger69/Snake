5171b0c634d212346ec39206748dca292cff89a9
Maks <52411040+Zinger69@users.noreply.github.com>
2020-04-06 19:22:33 +0300

    Add files via upload
    Работа со списками.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace Snake
 {
@@ -11,6 +13,26 @@ namespace Snake
 
             Point p2 = new Point(4, 5, '@');
             p2.Draw();
+
+            List<int> numList = new List<int>(); // создание списка
+            numList.Add(0);  // добваление елемента (целого числа) 0 в список numList
+            numList.Add(1);  // добваление елемента (целого числа) 1 в список numList
+            numList.Add(2);  // добваление елемента (целого числа) 2 в список numList
+
+            int x = numList[0]; //нумерация начинаеться с 0
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach (int i in numList) // чтобы пройти по всем елементам списка
+            {
+                Console.WriteLine(i);
+            }
+
+            numList.RemoveAt(0); //Удалить елемент с порядковым номером 0, то есть первый елемент спика 
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
 
             Console.WriteLine();
         }
