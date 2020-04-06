0c02c838df83090486a74c685202d983c2cb375a
Maks <52411040+Zinger69@users.noreply.github.com>
2020-04-06 22:21:41 +0300

    Add files via upload
    Snake move

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -22,5 +22,22 @@ namespace Snake
 				pList.Add(p);
 			}
 		}
+		internal void Move()
+		{
+			Point tail = pList.First();
+			pList.Remove(tail);
+			Point head = GetNextPoint();
+			pList.Add(head);
+
+			tail.Clear();
+			head.Draw();
+		}
+		public Point GetNextPoint()
+		{
+			Point head = pList.Last();
+			Point nextPoint = new Point(head);
+			nextPoint.Move(1, direction);
+			return nextPoint;
+		}
 	}
 }
