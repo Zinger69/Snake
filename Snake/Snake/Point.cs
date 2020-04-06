6895ae5d9726586efb3a4ba2088d0b144a50c549
Maks <52411040+Zinger69@users.noreply.github.com>
2020-04-06 18:22:13 +0300

    Add files via upload
    Функции Draw

---
+++ Snake/Snake/Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char sym;
+
+        public void Draw()  //Вместо того чтобы писать функцию в коде , можем прописать ее в классе.
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+}
